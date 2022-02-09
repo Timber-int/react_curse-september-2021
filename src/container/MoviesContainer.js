b21940ade4f10373746a9c3e377fef117184d34d
Artem <timoty01@ukr.net>
2022-02-09 12:29:08 +0200

    movie-app

--- src/container/MoviesContainer.js
+++ src/container/MoviesContainer.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {Layout} from '../components';
 import {MovieDetailPage, MoviesPage, NotFoundPage} from '../pages';
-import './MovieContainer.css';
 
 const MoviesContainer = () => {
     return (
