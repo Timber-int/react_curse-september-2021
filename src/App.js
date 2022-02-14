bb2efe8fb6817e8c61e860b6d0ba5787666351fc
Artem <timoty01@ukr.net>
2022-02-14 15:17:52 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
 
-
-function App() {
+const App = () => {
     return (
-        <div className="App">
-
+        <div>
+            
         </div>
     );
-}
+};
 
-export default App;
+export {App};
